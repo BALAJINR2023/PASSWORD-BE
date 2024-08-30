c657ce1eb166639927642d872b6eba42966c3460
BALAJINR2023 <ravibalajinr@gmail.com>
2024-08-30 09:23:55 +0530

    PASS BE

---
+++ db/Models.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    id: {
+      type: "string",
+      required: true,
+    },
+    name: {
+      type: "string",
+      required: true,
+    },
+    email: {
+      type: "string",
+      required: true,
+    },
+    password: {
+      type: "string",
+      required: true,
+    },
+    role: {
+      type: "string",
+      required: true,
+    },
+    isVerfied:{
+      type: "boolean",
+      required: false,
+    },
+  });
+  
+  const userModel = new mongoose.model("user", userSchema, "users");
+ export {userModel};
+  
---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './db/mongo.js';
+import MongooseconnectDB from './db/Mongoose.js';
+import registerRouter from './routes/auth/register.js';
+import loginRouter from './routes/auth/login.js';
+import forgotPasswordRouter from './routes/auth/forgotpass.js';
+import resetPasswordRouter from './routes/auth/reset.js';
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+await connectDB();
+await MongooseconnectDB();
+const customMiddleware = (req, res, next) => {
+    console.log(
+      new Date().toString(),
+      "Handling request for",
+      req.method,
+      req.originalUrl
+    );
+  
+    next();
+  };
+ 
+server.use(customMiddleware);
+
+
+server.use('/register', registerRouter);
+server.use('/login', loginRouter);
+server.use('/forgot-password', forgotPasswordRouter);
+server.use('/reset-password', resetPasswordRouter);
+
+const port = 8000;
+server.listen(port, ()=>{
+    console.log(Date().toString(),`Server is running on port ${port}`);
+});
