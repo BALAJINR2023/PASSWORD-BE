08dc016125f70ba3ceb541cf351e299165a431c4
BALAJINR2023 <ravibalajinr@gmail.com>
2024-08-30 18:05:25 +0530

    nill

--- db/Mongoose.js
+++ db/Mongoose.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 // const dbCluster="localhost:27017";    
 const dbCluster = process.env.DB_CULSTER||"task03.3a5m5.mongodb.net";
 const dbName = process.env.DB_NAME|| "";
